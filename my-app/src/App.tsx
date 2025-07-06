8ab15297a71c751763d5338c48beb9cf8a3b2cd0
samiunarno <samiunarno.iub@gmail.com>
2025-07-06 16:10:52 +0600

    new push

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,21 +1,7 @@
-// src/App.tsx
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { increment, decrement, reset } from './redux/features/counter/counter';
-import type { RootState } from './redux/store'; // Correct typing!
+import { Button } from "./components/ui/button";
 
-const App: React.FC = () => {
-  const count = useSelector((state: RootState) => state.counter);
-  const dispatch = useDispatch();
-
-  return (
-    <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>Counter: {count}</h1>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
-      <button onClick={() => dispatch(reset())}>Reset</button>
-    </div>
-  );
-};
+function App() {
+  return <Button>New Test</Button>;
+}
 
 export default App;
--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,11 +1,12 @@
 // main.tsx or index.tsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
