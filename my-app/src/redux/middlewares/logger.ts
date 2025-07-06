b5a85dbad596e8f832d473b5a896c8a3dea28158
samiunarno <samiunarno.iub@gmail.com>
2025-07-06 15:13:57 +0600

    new push

---
+++ my-app/src/redux/middlewares/logger.ts
@@ -0,0 +1,13 @@
+// const logger = (store) => (next) => (action) => {
+//   console.group(action.type);
+//   console.info("Previous State:", store.getState());
+
+//   const result = next(action);
+
+//   console.info("Next State:", store.getState());
+//   console.groupEnd();
+
+//   return result;
+// };
+
+// export default logger;
--- my-app/src/redux/store.ts
+++ my-app/src/redux/store.ts
@@ -1,13 +1,14 @@
 // src/redux/store.ts
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './features/counter/counter';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counter/counter";
+
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  
 });
-
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
